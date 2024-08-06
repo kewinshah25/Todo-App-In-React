a66ec9261c4c8a524269b28ad20f0a16ee5e1391
Kewin Shah <kewinshah25@gmail.com>
2024-08-06 01:51:56 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Todo-App-In-React/dist",
+  base: "/Todo-App-In-React",
   plugins: [react()],
 })
